963ad1badb0b2d84be4f08c342af5365eea472eb
Daniel Zhu <danielfangzhu@gmail.com>
2020-10-18 20:20:53 -0700

    Created basic flying mechanism.

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
@@ -7,7 +8,12 @@ public class Controller : MonoBehaviour
     [Tooltip("in ms^-1")] [SerializeField] float Speed = 20f;
     [Tooltip("in m")] [SerializeField] float xRange = 35f;
     [Tooltip("in m")] [SerializeField] float yRange = 35f;
+    [SerializeField] float positionPitchFactor = -19/7f;
+    [SerializeField] float controlPitchThrow = -15f;
+    [SerializeField] float positionYawFactor = 5/3f;
+    [SerializeField] float controlRollThrow = -15f;
     // Start is called before the first frame update
+    float xThrow, yThrow;
     void Start()
     {
         
@@ -16,8 +22,22 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        float pitch = transform.localPosition.y * positionPitchFactor + yThrow * controlPitchThrow;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollThrow;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
         float xOffset = xThrow * Speed * Time.deltaTime;
         float yOffset = yThrow * Speed * Time.deltaTime;
         float xPos = Mathf.Clamp(transform.localPosition.x + xOffset, -xRange, xRange);
