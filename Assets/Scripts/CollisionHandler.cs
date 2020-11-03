930440b5eeeb761bfbd795d1fd52589ca1270d27
Daniel Zhu <danielfangzhu@gmail.com>
2020-11-02 17:40:03 -0800

    Updated ship prefab and added death effects

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -2,16 +2,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [Tooltip("In seconds")][SerializeField] float levelLoadDelay = 1f;
+    [SerializeField] GameObject deathFX;
     private void OnTriggerEnter(Collider other)
     {
         StartDeathSequence();
+        deathFX.SetActive(true);
+        Invoke("reloadScene", levelLoadDelay);
     }
 
     private void StartDeathSequence()
     {
         SendMessage("flagDying");
+    }
+    private void reloadScene()
+    {
+        SceneManager.LoadScene(1);
     }
 }
