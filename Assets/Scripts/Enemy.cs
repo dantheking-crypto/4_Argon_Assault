9e2628f014cc40d6a4140f5274da71464c0a7883
Daniel Zhu <danielfangzhu@gmail.com>
2020-11-03 16:12:22 -0800

    Added explosion upon enemy destruction.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,13 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        BoxCollider collider = gameObject.AddComponent<BoxCollider>();
+        collider.isTrigger = false;
     }
 
     // Update is called once per frame
@@ -17,6 +21,8 @@ public class Enemy : MonoBehaviour
     }
     private void OnParticleCollision(GameObject other)
     {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
         Destroy(gameObject);
     }
 }
